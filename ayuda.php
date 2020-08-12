7556b37984173678e8aaf715806b5ada7e8d1411
Jhon Guanochanga <69156335+JhonGG-spec@users.noreply.github.com>
2020-08-12 13:20:39 -0500

    ayuda con tablas

--- ayuda.php
+++ ayuda.php
@@ -8,5 +8,13 @@
 <body>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ullam saepe nihil amet sequi dolorem fugiat hic ex deleniti totam officia, aliquam cumque incidunt similique ipsa quibusdam voluptates vel sapiente!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos temporibus aliquam, iste, corporis cupiditate necessitatibus repellendus sunt assumenda accusamus doloremque sint laborum eius minima. Molestias iusto magni dolorum praesentium repellat.</p>
+    <table>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
 </body>
 </html>
